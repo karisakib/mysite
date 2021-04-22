e972d80fbc66a675b099dd94a8e029331373afc1
Kai <kai@Kais-MBP.home>
2021-04-21 21:42:10 -0400

    added two new views

---
+++ mysite/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+# def index(request):
+#     return HttpResponse("Hello World")
+
+# view using loader()
+# def index(request):
+#     template = loader.get_template('index.html')
+#     return HttpResponse(template.render())
+
+# view using render()
+def index(request):
+    return render(request, 'index.html')
+
+def one(request):
+    return render(request, 'one.html')
+
+def two(request):
+    return render(request, 'two.html')
