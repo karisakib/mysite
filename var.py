05703ec46d744c296d7d2041a4dcb0a3c66d6094
karisakib <36942613+karisakib@users.noreply.github.com>
2021-05-06 06:25:44 -0400

    Delete var.py

--- var.py
+++
@@ -1,4 +0,0 @@
-skills = ['Python', 'NumPy', 'Pandas', 'SciPy', 'Matplotlib', 'Seaborn', 
-'Data Visualization', 'Data Cleaning', 'Data Manipulation', 'Probability / Statistics', 
-'RStudio', 'SQL', 'HTML / CSS / JavaScript', 'React', 'Java']
-
