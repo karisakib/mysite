c002828c5c910bc08a4ce0f9dd1f1dc7d3afa663
Kai <kai@Kais-MBP.home>
2021-05-11 10:34:51 -0400

    updated gitignore

---
+++ mysite/admin.py
@@ -0,0 +1,9 @@
+# from django.contrib import admin
+# from .models import ExampleModel
+
+# @admin.register(ExampleModel)
+# class ExampleModelAdmin(admin.ModelAdmin):
+#     # this would display all fields
+#     list_display = [field.name for field in ExampleModel._meta.get_fields()]
+#     # this would display specific fields
+#     list_display = ['name', 'email', 'ip_address']
